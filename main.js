47bfa66a356866a8576c95fb700c899d042f6b2c
Olimxoja77 <odilxojaevolimxoja@gmail.com>
2024-11-11 15:32:00 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+
+let item = ['anor','banan','apelsin','nok']
+let a=[]
+let o=[]
+
+for(b of items){
+    if(item.includes('a')){
+        a.push('a')
+    }else{
+        o.push('o')
+    }
+        
+}
+alert(item);
+alert(a);
+alert(o);
